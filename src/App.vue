3501fbcb157ac186e994ce9e9027a3e25fa34d24
Ti-tanium <1158976264@qq.com>
2018-08-21 14:46:04 +0800

    封装get方法
    使用async+await

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 
 
 <script>
+import  {get} from './utils/index' 
 export default{
-	created(){
+	async created(){
+		const res = await get('/weapp/test')
+		console.log(res,'hahahah')
+		console.log('小程序启动了')
+	}
+}
+</script>
+
+<style>
+
+</style>
+
 		console.log('小程序启动了')
 	}
 }
---
+++ src/utils/index.js
@@ -0,0 +1,23 @@
+// 工具函数库
+
+import config from '../config'
+
+export function get(url){
+    return new Promise((resolve,reject)=>{
+        wx.request({
+          url: config.host+url, //开发者服务器接口地址",
+          data: '', //请求的参数",
+          method: 'GET',
+          dataType: 'json', //如果设为json，会尝试对返回的数据做一次 JSON.parse
+          success: res => {
+              if(res.data.code==0){
+                  console.log('resolve',resolve(res.data.data))
+              }else{
+                  reject(res.data)
+              }
+          },
+          fail: () => {},
+          complete: () => {}
+        });
+    })
+}
