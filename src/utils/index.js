2d8ccce0d7850637a4f6020959f2f2a1541079bb
Ti-tanium <1158976264@qq.com>
2018-08-23 13:38:41 +0800

    utils add showSuccess and showModal

--- src/utils/index.js
+++ src/utils/index.js
@@ -21,3 +21,30 @@ export function get (url) {
     })
   })
 }
+
+export function showSuccess (text) {
+  wx.showToast({
+    title: text, // 提示的内容,
+    icon: 'success', // 图标,
+    duration: 2000, // 延迟时间,
+    mask: true, // 显示透明蒙层，防止触摸穿透,
+    success: res => {}
+  })
+}
+
+export function showModal (title, content) {
+  wx.showModal({
+    title: title, // 提示的标题,
+    content: content, // 提示的内容,
+    showCancel: true, // 是否显示取消按钮,
+    cancelText: '取消', // 取消按钮的文字，默认为取消，最多 4 个字符,
+    cancelColor: '#000000', // 取消按钮的文字颜色,
+    confirmText: '确定', // 确定按钮的文字，默认为取消，最多 4 个字符,
+    confirmColor: '#3CC51F', // 确定按钮的文字颜色,
+    success: res => {
+      if (res.confirm) {
+      } else if (res.cancel) {
+      }
+    }
+  })
+}
