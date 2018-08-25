c2eebd81c2d3bf6990868162e1a889e5ce42f4ab
Ti-tanium <1158976264@qq.com>
2018-08-25 17:17:46 +0800

    add view count

--- server/routes/index.js
+++ server/routes/index.js
@@ -41,4 +41,6 @@ router.post('/addbook', controllers.addbook)
 
 router.get('/booklist', controllers.booklist)
 
+router.get('/bookdetail', controllers.bookdetail)
+
 module.exports = router
--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -3,14 +3,22 @@
 </template>
 
 <script>
+import config from '@/config'
+import { get } from '@/utils/index'
 export default {
   data () {
     return {
       bookid: ''
     }
   },
+  methods: {
+    async getDetail () {
+      await get(config.host + '/weapp/bookdetail', { id: this.bookid })
+    }
+  },
   mounted () {
     this.bookid = this.$root.$mp.query.id
+    this.getDetail()
   }
 }
 </script>
