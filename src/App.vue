bf7967cbbb9380faa784849b7ebe67f055588dcf
Ti-tanium <1158976264@qq.com>
2018-08-25 16:47:08 +0800

    css adjustment

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script>
-import qcloud from "wafer2-client-sdk";
-import config from "./config";
+import qcloud from 'wafer2-client-sdk'
+import config from './config'
 export default {
-  async created() {
-    qcloud.setLoginUrl(config.loginUrl);
+  async created () {
+    qcloud.setLoginUrl(config.loginUrl)
   }
-};
+}
 </script>
 
 <style>
@@ -17,6 +17,12 @@ export default {
   height: 70rpx;
   line-height: 70rpx;
   font-size: 32rpx;
+}
+
+.page-footer {
+  text-align: center;
+  font-size: 12px;
+  color: #888;
 }
 
 .btn:active {
--- src/pages/books/index.vue
+++ src/pages/books/index.vue
@@ -2,7 +2,7 @@
 <template>
 <div class="container">
   <Card :key='book.id' v-for='book in books' :book='book'></Card>
-  <p class="footer" :hidden='hasMoreDataOfBook'>没有更多数据</p>
+  <p class="page-footer" :hidden='hasMoreDataOfBook'>没有更多数据</p>
 </div>
 
 </template>
@@ -28,9 +28,7 @@ export default {
       wx.showNavigationBarLoading()
       const books = await get(config.bookListUrl, { page: this.pageOfNewData })
       console.log('book getted:', books)
-      console.log(books.data.list.length)
       if (books.data.list.length <= 0) {
-        console.log('booklen==0')
         this.hasMoreDataOfBook = false
         return
       }
@@ -58,11 +56,6 @@ export default {
 <style>
 .container {
   background-color: #f2f2f2;
-}
-.footer {
-  color: #999;
-  text-align: center;
-  font-size: 12rpx;
 }
 </style>
 
