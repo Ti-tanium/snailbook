3d9617ea578c6de01676c47d6b0d6bd40ee1bb19
Ti-tanium <1158976264@qq.com>
2018-08-25 16:01:10 +0800

    add fetching data on reach bottom
    mysql .limit  .offset control which part of the data i get.

--- server/controllers/booklist.js
+++ server/controllers/booklist.js
@@ -1,10 +1,20 @@
 const { mysql } = require('../qcloud')
 
 module.exports = async ctx => {
+    const size = 5
+    const { page } = ctx.request.query
     const books = await mysql('book')
-        .select('*')
-        .orderBy('id', 'desc')
+        .join('cSessionInfo', 'book.openid', '=', 'cSessionInfo.open_id')
+        .select('book.*', 'cSessionInfo.user_info')
+        .limit(size)
+        .offset(page * size)
+        .orderBy('book.id', 'desc')
     ctx.state.data = {
-        list: books
+        list: books.map(v => {
+            const info = JSON.parse(v.user_info)
+            return Object.assign(v, {
+                nickName: info.nickName
+            })
+        })
     }
 }
--- src/pages/books/index.vue
+++ src/pages/books/index.vue
@@ -1,6 +1,8 @@
 
 <template>
 <div class="container">
+  <Card :key='book.id' v-for='book in books' :book='book'></Card>
+  <p class="footer" :hidden='hasMoreDataOfBook'>没有更多数据</p>
 </div>
 
 </template>
@@ -16,13 +18,25 @@ export default {
   },
   data () {
     return {
-      books: []
+      books: [],
+      hasMoreDataOfBook: true,
+      pageOfNewData: 0
     }
   },
   methods: {
     async getList () {
-      const books = await get(config.bookListUrl)
-      this.books = books.data.list
+      wx.showNavigationBarLoading()
+      const books = await get(config.bookListUrl, { page: this.pageOfNewData })
+      console.log('book getted:', books)
+      console.log(books.data.list.length)
+      if (books.data.list.length <= 0) {
+        console.log('booklen==0')
+        this.hasMoreDataOfBook = false
+        return
+      }
+      this.books = this.books.concat(books.data.list)
+      wx.stopPullDownRefresh()
+      wx.hideNavigationBarLoading()
     }
   },
   mounted () {
@@ -31,6 +45,12 @@ export default {
   onPullDownRefresh () {
     this.getList()
   },
+  onReachBottom () {
+    if (!this.hasMoreDataOfBook) {
+      return
+    }
+    this.pageOfNewData += 1
+    this.getList()
   }
 }
 </script>
@@ -38,6 +58,11 @@ export default {
 <style>
 .container {
   background-color: #f2f2f2;
+}
+.footer {
+  color: #999;
+  text-align: center;
+  font-size: 12rpx;
 }
 </style>
 
