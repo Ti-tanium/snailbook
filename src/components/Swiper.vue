f52dca223a0e5342ac5990d9d6a882326fc4c004
Ti-tanium <1158976264@qq.com>
2018-08-27 13:23:03 +0800

    add swiper
    display the top 9 popular book .
    however, failed to bind the click event listener to the img.

---
+++ src/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="siwperContainer">
+    <swiper indicator-dots='true' autoplay='true' circular='true'>
+      <div v-for="(book,idx) in imgUrls" :key='idx'>
+        <swiper-item>
+          <img
+          @click='bookDetail(img.id)'
+          :src="img.image"
+          mode='aspectFit'
+          class='swiperImg'
+          v-for="img in book"
+          :key='index'
+          >
+        </swiper-item>
+      </div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['top9Books'],
+  computed: {
+    imgUrls () {
+      let res = this.top9Books
+      return [res.slice(0, 3), res.slice(3, 6), res.slice(6)]
+    }
+  },
+  methods: {
+    bookDetail (id) {
+      console.log('click img')
+      wx.navigateTo({
+        url: '/pages/detail/main?id=' + id
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.swiperContainer {
+  margin-top: 5px;
+}
+.swiperImg {
+  width: 33%;
+  height: 100%;
+}
+</style>
+
--- src/pages/books/index.vue
+++ src/pages/books/index.vue
@@ -1,6 +1,7 @@
 
 <template>
 <div class="container">
+  <swiper :top9Books='top9Books'></swiper>
   <Card :key='book.id' v-for='book in books' :book='book'></Card>
   <p class="page-footer" :hidden='hasMoreDataOfBook'>没有更多数据</p>
 </div>
@@ -12,47 +13,73 @@
 import config from '@/config'
 import { get } from '@/utils/index'
 import Card from '@/components/Card'
+import Swiper from '@/components/Swiper'
 export default {
   components: {
-    Card
+    Card,
+    Swiper
   },
   data () {
     return {
       books: [],
       hasMoreDataOfBook: true,
       pageIndex: 0,
-      pageSize: 5
+      pageSize: 10,
+      top9Books: []
     }
   },
   methods: {
-    async getList () {
+    async getList (pageSize) {
       wx.showNavigationBarLoading()
       const books = await get(config.bookListUrl, {
         pageIndex: this.pageIndex,
-        pageSize: this.pageSize
+        pageSize: pageSize
       })
       console.log('book getted:', books)
       if (books.data.list.length <= 0) {
         this.hasMoreDataOfBook = false
+        wx.hideNavigationBarLoading()
+        wx.stopPullDownRefresh()
         return
       }
       this.books = this.books.concat(books.data.list)
+      this.top9Books = this.books.slice(0, 9)
+      console.log('books:', this.books)
       wx.stopPullDownRefresh()
       wx.hideNavigationBarLoading()
     }
   },
+
   mounted () {
-    this.getList()
+    this.getList(this.pageSize)
   },
-  onPullDownRefresh () {
-    this.getList()
+  async onPullDownRefresh () {
+    this.pageIndex = 0
+    this.hasMoreDataOfBook = true
+    wx.showNavigationBarLoading()
+    const books = await get(config.bookListUrl, {
+      pageIndex: this.pageIndex,
+      pageSize: this.pageSize
+    })
+    console.log('book getted:', books)
+    if (books.data.list.length <= 0) {
+      this.hasMoreDataOfBook = false
+      wx.hideNavigationBarLoading()
+      wx.stopPullDownRefresh()
+      return
+    }
+    this.books = books.data.list
+    this.top9Books = this.books.slice(0, 9)
+    wx.stopPullDownRefresh()
+    wx.hideNavigationBarLoading()
   },
+
   onReachBottom () {
     if (!this.hasMoreDataOfBook) {
       return
     }
     this.pageIndex += 1
-    this.getList()
+    this.getList(this.pageSize)
   }
 }
 </script>
